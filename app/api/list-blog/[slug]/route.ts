2698e0a56796ce5b47805b536c6ec03a39dd21ca
SocialBotdev <monkon.official@gmail.com>
2025-06-28 08:37:44 +0530

    resolved type errors

--- app/api/list-blog/[slug]/route.ts
+++ app/api/list-blog/[slug]/route.ts
@@ -2,13 +2,13 @@ import { type NextRequest, NextResponse } from "next/server";
 import dbConnect from "@/lib/mongoose";
 import Blog from "@/models/list-blog";
 
-export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
+export async function GET(request: NextRequest, context: { params: { slug: string } }) {
   try {
     // Connect to the database
     await dbConnect();
 
     // Fetch the blog post by slug
-    const blog = await Blog.findOne({ slug: params.slug });
+    const blog = await Blog.findOne({ slug: context.params.slug });
 
     if (!blog) {
       return NextResponse.json({ error: "Blog not found" }, { status: 404 });
